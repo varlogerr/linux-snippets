f3542e1278ebd094581df3db9ca43ae721840eb6
Varlog Err <varlog.err@gmail.com>
2023-02-07 11:29:52 +0200

    fix enable ssh service

--- .ls-tools/bin/ls.linux.ansible-target.sh
+++ .ls-tools/bin/ls.linux.ansible-target.sh
@@ -18,12 +18,16 @@ __bootstrap_iife() {
 }; __bootstrap_iife; unset __bootstrap_iife
 
 ansible_target_install() {
-  (
-    set -x
-    apt-get update
-    apt-get install -y "${TOOLS[@]}"
-    systemctl enable --now sshd
-  )
+  (set -x; apt-get update; apt-get install -y "${TOOLS[@]}")
+
+  local service_name; service_name="$(
+    systemctl list-units --type service --all --plain --quiet \
+    | grep -o '^\s*sshd\?\.service' \
+    | sed -e 's/^\s*//' -e 's/\s*$//' | head -n 1 | cut -d' ' -f1
+  )"
+  service_name="${service_name:-sshd.service}"
+
+  (set -x; systemctl enable --now "${service_name}")
 }
 
 ansible_target_install
